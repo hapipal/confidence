8f9f55c9b0e60e3391dc6fdb17dc446442d158ef
Eran Hammer <eran@hueniverse.com>
2013-09-03 00:09:12 -0700

    write

--- test/store.js
+++ test/store.js
@@ -51,48 +51,129 @@ describe('Confidence', function () {
             }
         };
 
-        var store = new Store();
-        store.load(tree);
+        describe('#get', function () {
 
-        var get = function (key, value, criteria, depth) {
+            var store = new Store();
+            store.load(tree);
 
-            it('gets value for ' + key + (criteria ? ' with criteria ' + JSON.stringify(criteria) : ''), function (done) {
+            var get = function (key, result, criteria, depth) {
 
-                var result = store.get(key, criteria || {}, depth);
-                expect(result).to.deep.equal(value);
-                done();
+                it('gets value for ' + key + (criteria ? ' with criteria ' + JSON.stringify(criteria) : ''), function (done) {
+
+                    store.get(key, criteria || {}, depth, function (err, value) {
+
+                        expect(value).to.deep.equal(result);
+                        done();
+                    });
+                });
+            };
+
+            get('key', undefined);
+            get('/key1', 'abc');
+            get('/key2', 2);
+            get('/key2', 1, { platform: 'ios' });
+            get('/key2/deeper', 'value', { env: 'production' });
+            get('/key2/deeper', null, { env: 'qa' });
+            get('/key2/deeper', null);
+            get('/', { key1: 'abc', key2: 2, key3: { sub1: 123 } });
+            get('/', { key1: 'abc', key2: 2, key3: { sub1: 123, sub2: 6 } }, { xfactor: 'yes' });
+            get('/', { key1: 'abc', key2: 2, key3: {} }, null, 1);
+            get('/', { key1: 'abc', key2: 2, key3: { sub1: 123 } }, null, 2);
+        });
+
+        describe('#read', function () {
+
+            var store = new Store();
+            store.load(tree);
+
+            var read = function (key, result) {
+
+                it('reads value for ' + key, function (done) {
+
+                    store.read(key, function (err, value) {
+
+                        expect(value).to.deep.equal(result);
+                        done();
+                    });
+                });
+            };
+
+            read('key1', undefined);
+            read('/', tree);
+            read('/key1', 'abc');
+            read('/key2/production/deeper', 'value');
+            read('/key2/$default/$filter', 'platform');
+            read('/key3/sub2', { $filter: 'xfactor', yes: 6 });
+            read('/key4', null);
+        });
+
+        describe('#write', function () {
+
+            it('writes root node', function (done) {
+
+                var store = new Store();
+                store.write('/', { key1: 1, key2: 2 }, function (err) {
+
+                    expect(err).to.not.exist;
+                    done();
+                });
             });
-        };
 
-        get('key', null);
-        get('/key1', 'abc');
-        get('/key2', 2);
-        get('/key2', 1, { platform: 'ios' });
-        get('/key2/deeper', 'value', { env: 'production' });
-        get('/key2/deeper', null, { env: 'qa' });
-        get('/key2/deeper', null);
-        get('/', { key1: 'abc', key2: 2, key3: { sub1: 123 } });
-        get('/', { key1: 'abc', key2: 2, key3: { sub1: 123, sub2: 6 } }, { xfactor: 'yes' });
-        get('/', { key1: 'abc', key2: 2, key3: { } }, null, 1);
-        get('/', { key1: 'abc', key2: 2, key3: { sub1: 123 } }, null, 2);
+            it('errors on invalid node', function (done) {
 
-        var read = function (key, value) {
+                var store = new Store();
+                store.write('/', null, function (err) {
 
-            it('reads value for ' + key, function (done) {
-
-                var result = store.read(key);
-                expect(result).to.deep.equal(value);
-                done();
+                    expect(err.message).to.equal('Node cannot be null or undefined');
+                    done();
+                });
             });
-        };
 
-        read('key1', null);
-        read('/', tree);
-        read('/key1', 'abc');
-        read('/key2/production/deeper', 'value');
-        read('/key2/$default/$filter', 'platform');
-        read('/key3/sub2', { $filter: 'xfactor', yes: 6 });
-        read('/key4', null);
+            it('errors on invalid key', function (done) {
+
+                var store = new Store();
+                store.write('/$a', 'value', function (err) {
+
+                    expect(err.message).to.equal('Bad key segment: /$a');
+                    done();
+                });
+            });
+
+            it('errors on missing key path', function (done) {
+
+                var store = new Store();
+                store.write('/a/b', 'value', function (err) {
+
+                    expect(err.message).to.equal('Key path does not exist');
+                    done();
+                });
+            });
+
+            it('adds node', function (done) {
+
+                var store = new Store();
+                store.write('/', { key1: 1, key2: 2 }, function (err) {
+
+                    expect(err).to.not.exist;
+
+                    store.read('/key2', function (err, value) {
+
+                        expect(err).to.not.exist;
+                        expect(value).to.equal(2);
+
+                        store.write('/key3', { $filter: 'x', a: 1, $default: 2 }, function (err) {
+
+                            expect(err).to.not.exist;
+                            store.get('/key3', { x: 'a' }, 1, function (err, value) {
+
+                                expect(value).to.deep.equal(1);
+                                done();
+                            });
+                        });
+                    });
+                });
+            });
+        });
 
         describe('#load', function () {
 
