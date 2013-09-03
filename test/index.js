4335645f28ce39e050c55a36b3b374b5faa0634f
Eran Hammer <eran@hueniverse.com>
2013-09-03 10:36:04 -0700

    100%

--- test/index.js
+++ test/index.js
@@ -22,11 +22,33 @@ describe('Confidence', function () {
 
     describe('Plugin', function () {
 
-        var server = new Hapi.Server(0);
+        var server = new Hapi.Server(0, { labels: ['api', 'admin'] });
 
         before(function (done) {
 
-            server.pack.require('../', {}, function (err) {
+            var tree = {
+                key1: 'abc',
+                key2: {
+                    $filter: 'env',
+                    production: {
+                        deeper: 'value'
+                    },
+                    $default: {
+                        $filter: 'platform',
+                        ios: 1,
+                        $default: 2
+                    }
+                },
+                key3: {
+                    sub1: 123,
+                    sub2: {
+                        $filter: 'xfactor',
+                        yes: 6
+                    }
+                }
+            };
+
+            server.pack.require('../', { tree: tree }, function (err) {
 
                 expect(err).to.not.exist;
                 server.start(function () {
@@ -36,14 +58,51 @@ describe('Confidence', function () {
             });
         });
 
-        //it('returns root', function (done) {
+        it('issues id', function (done) {
 
-        //    server.inject('/experimental/dat/department', function (res) {
+            server.inject({ method: 'POST', url: '/id', payload: {} }, function (res) {
 
-        //        expect(res.statusCode).to.equal(200);
-        //        expect(res.result.children.length).to.equal(31);
-        //        done();
-        //    });
-        //});
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
+        });
+
+        it('gets key', function (done) {
+
+            server.inject('/key/12345/key1', function (res) {
+
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
+        });
+
+        it('reads raw key', function (done) {
+
+            server.inject('/node/key1', function (res) {
+
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
+        });
+
+        it('writes node', function (done) {
+
+            server.inject({ method: 'POST', url: '/node/key1', payload: {} }, function (res) {
+
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
+        });
+
+        it('fails loading bad tree', function (done) {
+
+            var server = new Hapi.Server(0, { labels: ['api', 'admin'] });
+
+            server.pack.require('../', { tree: { $default: 1 } }, function (err) {
+
+                expect(err.message).to.equal('Default value without a filter');
+                done();
+            });
+        });
     });
 });
